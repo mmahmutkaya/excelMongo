7781b1e51f798a9a292bfd94780c22a643f8a6ff
Mahmut Kaya <mmahmutkaya@gmail.com>
2025-08-03 19:57:02 +0500

    aa

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+// app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+// app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
+// module.exports = app;
