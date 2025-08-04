604ae10fa9738452bbc6d1c3fe3b59c15244b18b
Mahmut Kaya <mmahmutkaya@gmail.com>
2025-08-05 01:24:53 +0500

    aa

--- models/pozModel.js
+++ models/pozModel.js
@@ -3,11 +3,31 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const pozSchema = new Schema({
+  _firmaId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  _projeId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  _wbsId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+  pozNo: {
+    type: String,
+    required: true
+  },
   pozName: {
     type: String,
     required: true
   },
-  pozNo: {
+  pozBirimId: {
+    type: String,
+    required: true
+  },
+  pozMetrajTipId: {
     type: String,
     required: true
   }
