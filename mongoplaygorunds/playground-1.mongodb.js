75fc9e4575d4d428d31946d3cec9f230ebfd2e3a
Mahmut Kaya <mmahmutkaya@gmail.com>
2025-08-05 01:11:50 +0500

    aa

---
+++ mongoplaygorunds/playground-1.mongodb.js
@@ -0,0 +1,21 @@
+
+use('rapor724_v2');
+
+
+
+// db.getCollection('pozlar').deleteMany({_projeId:{ $exists: false }})
+
+db.getCollection('pozlar').find({})
+
+// Print a message to the output window.
+// console.log(`${salesOnApril4th} sales occurred in 2014.`);
+
+// Here we run an aggregation and open a cursor to the results.
+// Use '.toArray()' to exhaust the cursor to return the whole result set.
+// You can use '.hasNext()/.next()' to iterate through the cursor page by page.
+// db.getCollection('sales').aggregate([
+//   // Find all of the sales that occurred in 2014.
+//   { $match: { date: { $gte: new Date('2014-01-01'), $lt: new Date('2015-01-01') } } },
+//   // Group the total sales for each product.
+//   { $group: { _id: '$item', totalSaleAmount: { $sum: { $multiply: [ '$price', '$quantity' ] } } } }
+// ]);
