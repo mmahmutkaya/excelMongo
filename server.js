05709adf945429cde848684a4e8725b66cd320e9
Mahmut Kaya <mmahmutkaya@gmail.com>
2025-10-02 00:34:23 +0500

    aa

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const pozRoutes = require('./routes/pozlar')
+const userRoutes = require('./routes/user')
 
 // express app
 const app = express()
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/pozlar', pozRoutes)
+app.use('/api/user', userRoutes)
 
 // mongoose.set('strictQuery', true)
 
