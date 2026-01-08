11dd04e6c2b40df909837dc590aa7af2949de509
Mahmut Kaya <mmahmutkaya@gmail.com>
2026-01-08 16:25:08 +0300

    aa

--- server.js
+++ server.js
@@ -24,13 +24,14 @@ app.use(express.json())
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  res.header("Access-Control-Allow-Headers", "*");
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  // res.header('Access-Control-Allow-Origin', req.headers.origin);
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  // res.header("Access-Control-Allow-Headers", "*");
+  // res.header('Access-Control-Allow-Credentials', true);
+  // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   next()
 })
+
 
 // routes
 app.use('/api/pozlar', pozRoutes)
