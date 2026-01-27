4e72de469d6c622e4e7ce7f8a7fa395aebd333d1
Mahmut Kaya <mmahmutkaya@gmail.com>
2026-01-27 17:58:04 +0300

    aa

--- controllers/versiyonController.js
+++ controllers/versiyonController.js
@@ -9,162 +9,162 @@ var ObjectId = require('mongodb').ObjectId;
 
 
 
-const createVersiyon_metraj = async (req, res) => {
+// const createVersiyon_metraj = async (req, res) => {
 
-  const hataBase = "BACKEND - (createVersiyon_metraj) - "
+//   const hataBase = "BACKEND - (createVersiyon_metraj) - "
 
-  try {
+//   try {
 
-    const currentTime = new Date();
+//     const currentTime = new Date();
 
-    const {
-      email: userEmail,
-      isim: userIsim,
-      soyisim: userSoyisim,
-      userCode
-    } = JSON.parse(req.user)
-
-
-    const { projeId, pozlar_metraj, versiyonNumber, aciklama } = req.body
+//     const {
+//       email: userEmail,
+//       isim: userIsim,
+//       soyisim: userSoyisim,
+//       userCode
+//     } = JSON.parse(req.user)
 
 
-    let _projeId
-    try {
-      _projeId = new ObjectId(projeId)
-    } catch (error) {
-      throw new Error("DB ye gönderilen 'projeId' verisi geçerli bir BSON ObjectId verisine dönüşemedi, sayfayı yenileyiniz, sorun devam ederse Rapor7/24 ile irtibata geçiniz.")
-    }
-
-    if (!pozlar_metraj.length > 0) {
-      throw new Error("'pozlar_metraj' verisi db sorgusuna gelmedi")
-    }
-
-    if (!versiyonNumber) {
-      throw new Error("'versiyonNumber' verisi db sorgusuna gelmedi")
-    }
+//     const { projeId, pozlar_metraj, versiyonNumber, aciklama } = req.body
 
 
-    let theProje = await Proje.findOne({ _id: projeId })
-    if (!theProje) {
-      throw new Error("sorguya gönderilen 'collectionId' ile sistemde 'document' bulunamadı, lütfen sayfayı yenileyiniz, sorun devam ederse Rapor7/24 ile irtibata geçiniz.")
-    }
+//     let _projeId
+//     try {
+//       _projeId = new ObjectId(projeId)
+//     } catch (error) {
+//       throw new Error("DB ye gönderilen 'projeId' verisi geçerli bir BSON ObjectId verisine dönüşemedi, sayfayı yenileyiniz, sorun devam ederse Rapor7/24 ile irtibata geçiniz.")
+//     }
+
+//     if (!pozlar_metraj.length > 0) {
+//       throw new Error("'pozlar_metraj' verisi db sorgusuna gelmedi")
+//     }
+
+//     if (!versiyonNumber) {
+//       throw new Error("'versiyonNumber' verisi db sorgusuna gelmedi")
+//     }
 
 
-    // yetki kontrol
-    try {
-      let arananYetkiler = ["birimFiyatEdit", "owner"]
-
-      let hasYetki
-      arananYetkiler.map(oneAranan => {
-        theProje.yetkiliKisiler.find(x => x.email === userEmail)?.yetkiler?.map(oneYetki => {
-          if (oneYetki.name === oneAranan) {
-            hasYetki = true
-          }
-        })
-      })
-      if (!hasYetki) {
-        return res.status(200).json({ message: "Bu işlem için yetkiniz yok görünüyor, Rapor7/24 ile iletişime geçebilirsiniz." })
-      }
+//     let theProje = await Proje.findOne({ _id: projeId })
+//     if (!theProje) {
+//       throw new Error("sorguya gönderilen 'collectionId' ile sistemde 'document' bulunamadı, lütfen sayfayı yenileyiniz, sorun devam ederse Rapor7/24 ile irtibata geçiniz.")
+//     }
 
 
-    } catch (error) {
-      throw new Error("tryCatch -yetki- " + error)
-    }
+//     // yetki kontrol
+//     try {
+//       let arananYetkiler = ["birimFiyatEdit", "owner"]
+
+//       let hasYetki
+//       arananYetkiler.map(oneAranan => {
+//         theProje.yetkiliKisiler.find(x => x.email === userEmail)?.yetkiler?.map(oneYetki => {
+//           if (oneYetki.name === oneAranan) {
+//             hasYetki = true
+//           }
+//         })
+//       })
+//       if (!hasYetki) {
+//         return res.status(200).json({ message: "Bu işlem için yetkiniz yok görünüyor, Rapor7/24 ile iletişime geçebilirsiniz." })
+//       }
 
 
-
-    let proje = await Proje.findOne({ _id: _projeId })
-    const { metrajVersiyonlar } = proje
+//     } catch (error) {
+//       throw new Error("tryCatch -yetki- " + error)
+//     }
 
 
 
-
-    try {
-
-      await Dugum.updateMany({ _projeId }, [
-        {
-          $set: {
-            hazirlananMetrajlar: {
-              $map: {
-                input: "$hazirlananMetrajlar",
-                as: "oneHazirlanan",
-                in: {
-                  $mergeObjects: [
-                    "$$oneHazirlanan",
-                    {
-                      satirlar: {
-                        $map: {
-                          input: "$$oneHazirlanan.satirlar",
-                          as: "oneSatir",
-                          in: {
-                            $cond: {
-                              if: { $eq: ["$$oneSatir.versiyon", 0] },
-                              else: "$$oneSatir",
-                              then: {
-                                $mergeObjects: [
-                                  "$$oneSatir",
-                                  { versiyon: versiyonNumber }
-                                ]
-                              }
-                            }
-                          }
-                        }
-                      }
-                    }
-                  ]
-                }
-              }
-            },
-            revizeMetrajlar: {
-              $map: {
-                input: "$revizeMetrajlar",
-                as: "oneMetraj",
-                in: {
-                  $mergeObjects: [
-                    "$$oneMetraj",
-                    {
-                      satirlar: {
-                        $map: {
-                          input: "$$oneMetraj.satirlar",
-                          as: "oneSatir",
-                          in: {
-                            $cond: {
-                              if: { $eq: ["$$oneSatir.versiyon", 0] },
-                              else: "$$oneSatir",
-                              then: {
-                                $mergeObjects: [
-                                  "$$oneSatir",
-                                  { versiyon: versiyonNumber }
-                                ]
-                              }
-                            }
-                          }
-                        }
-                      }
-                    }
-                  ]
-                }
-              }
-            },
-            metrajVersiyonlar: { $concatArrays: ["$metrajVersiyonlar", [{ versiyonNumber, metrajOnaylanan: "$metrajOnaylanan" }]] }
-          }
-        }
-      ])
+//     let proje = await Proje.findOne({ _id: _projeId })
+//     const { metrajVersiyonlar } = proje
 
 
-    } catch (error) {
-      throw new Error("tryCatch -2- " + error);
-    }
 
-    await proje.metrajVersiyonlar.push({ versiyonNumber, createdAt: currentTime })
 
-    return res.status(200).json({ proje })
+//     try {
 
-  } catch (error) {
-    return res.status(400).json({ error: hataBase + error })
-  }
+//       await Dugum.updateMany({ _projeId }, [
+//         {
+//           $set: {
+//             hazirlananMetrajlar: {
+//               $map: {
+//                 input: "$hazirlananMetrajlar",
+//                 as: "oneHazirlanan",
+//                 in: {
+//                   $mergeObjects: [
+//                     "$$oneHazirlanan",
+//                     {
+//                       satirlar: {
+//                         $map: {
+//                           input: "$$oneHazirlanan.satirlar",
+//                           as: "oneSatir",
+//                           in: {
+//                             $cond: {
+//                               if: { $eq: ["$$oneSatir.versiyon", 0] },
+//                               else: "$$oneSatir",
+//                               then: {
+//                                 $mergeObjects: [
+//                                   "$$oneSatir",
+//                                   { versiyon: versiyonNumber }
+//                                 ]
+//                               }
+//                             }
+//                           }
+//                         }
+//                       }
+//                     }
+//                   ]
+//                 }
+//               }
+//             },
+//             revizeMetrajlar: {
+//               $map: {
+//                 input: "$revizeMetrajlar",
+//                 as: "oneMetraj",
+//                 in: {
+//                   $mergeObjects: [
+//                     "$$oneMetraj",
+//                     {
+//                       satirlar: {
+//                         $map: {
+//                           input: "$$oneMetraj.satirlar",
+//                           as: "oneSatir",
+//                           in: {
+//                             $cond: {
+//                               if: { $eq: ["$$oneSatir.versiyon", 0] },
+//                               else: "$$oneSatir",
+//                               then: {
+//                                 $mergeObjects: [
+//                                   "$$oneSatir",
+//                                   { versiyon: versiyonNumber }
+//                                 ]
+//                               }
+//                             }
+//                           }
+//                         }
+//                       }
+//                     }
+//                   ]
+//                 }
+//               }
+//             },
+//             metrajVersiyonlar: { $concatArrays: ["$metrajVersiyonlar", [{ versiyonNumber, metrajOnaylanan: "$metrajOnaylanan" }]] }
+//           }
+//         }
+//       ])
 
-}
+
+//     } catch (error) {
+//       throw new Error("tryCatch -2- " + error);
+//     }
+
+//     await proje.metrajVersiyonlar.push({ versiyonNumber, createdAt: currentTime })
+
+//     return res.status(200).json({ proje })
+
+//   } catch (error) {
+//     return res.status(400).json({ error: hataBase + error })
+//   }
+
+// }
 
 
 
@@ -214,6 +214,227 @@ const createVersiyon_metraj = async (req, res) => {
 
 
 
+const createVersiyon_metraj = async (req, res) => {
+
+  const hataBase = "BACKEND - (createVersiyon_metraj) - "
+
+
+  try {
+
+    const currentTime = new Date()
+
+    const {
+      email: userEmail,
+      isim: userIsim,
+      soyisim: userSoyisim,
+      userCode
+    } = JSON.parse(req.user)
+
+    const { projeId, pozlar, versiyonNumber, aciklama } = req.body
+
+    let _projeId
+    try {
+      _projeId = new ObjectId(projeId)
+    } catch (error) {
+      throw new Error("DB ye gönderilen 'projeId' verisi geçerli bir BSON ObjectId verisine dönüşemedi, sayfayı yenileyiniz, sorun devam ederse Rapor7/24 ile irtibata geçiniz.")
+    }
+
+    if (!pozlar.length > 0) {
+      throw new Error("'pozlar' verisi db sorgusuna gelmedi");
+    }
+
+    if (!versiyonNumber) {
+      throw new Error("'versiyonNumber' verisi db sorgusuna gelmedi");
+    }
+
+
+    let theProje = await Proje.findOne({ _id: projeId })
+    if (!theProje) {
+      throw new Error("sorguya gönderilen 'collectionId' ile sistemde 'document' bulunamadı, lütfen sayfayı yenileyiniz, sorun devam ederse Rapor7/24 ile irtibata geçiniz.")
+    }
+
+
+
+    // yetki kontrol
+    try {
+      if (!theProje.aktifYetkiliKisiler.find(x => x.yetki === "metrajOnay" && x.email === userEmail)) {
+        return res.status(200).json({ message: "Gerekli süre içerisinde işlem yapmadınız ya da bu işlem için yetkiniz yok, sayfayı yenileyip tekrar deneyiniz, Rapor7/24 ile iletişime geçebilirsiniz." })
+      }
+    } catch (error) {
+      throw new Error("tryCatch -yetki- " + error)
+    }
+
+
+
+
+    // try {
+
+    //   await Dugum.updateMany({ _projeId }, [
+    //     {
+    //       $set: {
+    //         hazirlananMetrajlar: {
+    //           $map: {
+    //             input: "$hazirlananMetrajlar",
+    //             as: "oneHazirlanan",
+    //             in: {
+    //               $mergeObjects: [
+    //                 "$$oneHazirlanan",
+    //                 {
+    //                   satirlar: {
+    //                     $map: {
+    //                       input: "$$oneHazirlanan.satirlar",
+    //                       as: "oneSatir",
+    //                       in: {
+    //                         $cond: {
+    //                           if: { $eq: ["$$oneSatir.versiyon", 0] },
+    //                           else: "$$oneSatir",
+    //                           then: {
+    //                             $mergeObjects: [
+    //                               "$$oneSatir",
+    //                               { versiyon: versiyonNumber }
+    //                             ]
+    //                           }
+    //                         }
+    //                       }
+    //                     }
+    //                   }
+    //                 }
+    //               ]
+    //             }
+    //           }
+    //         },
+    //         revizeMetrajlar: {
+    //           $map: {
+    //             input: "$revizeMetrajlar",
+    //             as: "oneMetraj",
+    //             in: {
+    //               $mergeObjects: [
+    //                 "$$oneMetraj",
+    //                 {
+    //                   satirlar: {
+    //                     $map: {
+    //                       input: "$$oneMetraj.satirlar",
+    //                       as: "oneSatir",
+    //                       in: {
+    //                         $cond: {
+    //                           if: { $eq: ["$$oneSatir.versiyon", 0] },
+    //                           else: "$$oneSatir",
+    //                           then: {
+    //                             $mergeObjects: [
+    //                               "$$oneSatir",
+    //                               { versiyon: versiyonNumber }
+    //                             ]
+    //                           }
+    //                         }
+    //                       }
+    //                     }
+    //                   }
+    //                 }
+    //               ]
+    //             }
+    //           }
+    //         },
+    //         metrajVersiyonlar: { $concatArrays: ["$metrajVersiyonlar", [{ versiyonNumber, metrajOnaylanan: "$metrajOnaylanan" }]] }
+    //       }
+    //     }
+    //   ])
+
+
+    // } catch (error) {
+    //   throw new Error("tryCatch -1- " + error);
+    // }
+
+
+
+
+    try {
+
+      const bulkArray1 = pozlar.map(onePoz => {
+
+        return (
+          {
+            updateOne: {
+              filter: { _id: onePoz._id },
+              update: {
+                $push: {
+                  metrajVersiyonlar: {
+                    versiyonNumber,
+                    metrajOnaylanan: onePoz.metrajOnaylanan,
+                    isProgress: onePoz?.hasVersiyonZero ? true : false
+                  }
+                }
+              }
+            }
+          }
+        )
+
+        // return (
+        //   {
+        //     updateOne: {
+        //       filter: { _id: onePoz._id },
+        //       update: {
+        //         $set: { birimFiyatlar: birimFiyatlar_noProgress },
+        //         $push: { birimFiyatVersiyonlar: { versiyonNumber, birimFiyatlar: birimFiyatlar_saveAs_versiyon } }
+        //       }
+        //     }
+        //   }
+        // )
+
+      })
+
+      // return res.status(200).json({ bulkArray1 })
+
+      await Poz.bulkWrite(
+        bulkArray1,
+        { ordered: false }
+      )
+
+
+    } catch (error) {
+      throw new Error("tryCatch -2- " + error)
+    }
+
+
+
+    try {
+
+      await Proje.updateOne({ _id: _projeId }, [
+        {
+          $set: {
+            metrajVersiyonlar: {
+              $concatArrays: [
+                "$metrajVersiyonlar",
+                [{ versiyonNumber, aciklama, createdAt: currentTime, createdBy: userEmail }]
+              ]
+            },
+            aktifYetkiliKisiler: {
+              $filter: {
+                input: "$aktifYetkiliKisiler",
+                as: "oneAktif",
+                cond: { $ne: ["$$oneAktif.yetki", "metrajOnay"] }
+              }
+            }
+          }
+        }
+      ])
+
+    } catch (error) {
+      throw new Error("tryCatch -3- " + error)
+    }
+
+
+
+    return res.status(200).json({ ok: true })
+
+  } catch (error) {
+    return res.status(400).json({ error: hataBase + error })
+  }
+
+}
+
+
+
+
 
 const createVersiyon_birimFiyat = async (req, res) => {
 
@@ -258,21 +479,9 @@ const createVersiyon_birimFiyat = async (req, res) => {
 
     // yetki kontrol
     try {
-      let arananYetkiler = ["birimFiyatEdit", "owner"]
-
-      let hasYetki
-      arananYetkiler.map(oneAranan => {
-        theProje.yetkiliKisiler.find(x => x.email === userEmail)?.yetkiler?.map(oneYetki => {
-          if (oneYetki.name === oneAranan) {
-            hasYetki = true
-          }
-        })
-      })
-      if (!hasYetki) {
-        return res.status(200).json({ message: "Bu işlem için yetkiniz yok görünüyor, Rapor7/24 ile iletişime geçebilirsiniz." })
+      if (!theProje.aktifYetkiliKisiler.find(x => x.yetki === "birimFiyatEdit" && x.email === userEmail)) {
+        return res.status(200).json({ message: "Gerekli süre içerisinde işlem yapmadınız ya da bu işlem için yetkiniz yok, sayfayı yenileyip tekrar deneyiniz, Rapor7/24 ile iletişime geçebilirsiniz." })
       }
-
-
     } catch (error) {
       throw new Error("tryCatch -yetki- " + error)
     }
@@ -342,6 +551,14 @@ const createVersiyon_birimFiyat = async (req, res) => {
                   "$birimFiyatVersiyonlar",
                   [{ versiyonNumber, aciklama, createdAt: currentTime, createdBy: userEmail }]
                 ]
+              },
+              birimFiyatVersiyon_isProgress: false,
+              aktifYetkiliKisiler: {
+                $filter: {
+                  input: "$aktifYetkiliKisiler",
+                  as: "oneAktif",
+                  cond: { $ne: ["$$oneAktif.yetki", "birimFiyatEdit"] }
+                }
               }
             }
           }
@@ -358,7 +575,14 @@ const createVersiyon_birimFiyat = async (req, res) => {
         await Proje.updateOne({ _id: _projeId }, [
           {
             $set: {
-              birimFiyatVersiyon_isProgress: false
+              birimFiyatVersiyon_isProgress: false,
+              aktifYetkiliKisiler: {
+                $filter: {
+                  input: "$aktifYetkiliKisiler",
+                  as: "oneAktif",
+                  cond: { $ne: ["$$oneAktif.yetki", "birimFiyatEdit"] }
+                }
+              }
             }
           }
         ])
